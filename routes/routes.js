2d5bf8b3bed5aba13e92c1a8e7b73421a9ec6b7c
martinjr1980 <martinjr1980@gmail.com>
2014-11-25 17:20:52 -0800

    recommit adding catchit

---
+++ routes/routes.js
@@ -0,0 +1,29 @@
+module.exports = function(app){
+	app.get('/', function(req, res) {
+		res.render('index');
+	});
+
+	app.get('/catchit', function(req, res) {
+		res.render('catchit');
+	});
+
+	app.get('/catchit_game', function(req, res) {
+		res.render('catchit_game');
+	})
+
+	app.get('/matchit', function(req, res) {
+		res.render('matchit');
+	});
+
+	app.get('/popit', function(req, res) {
+		res.render('popit');
+	});
+
+	app.get('/popit_start', function(req, res) {
+		res.render('popit_start');
+	});
+
+	app.get('/popit_level2', function(req, res) {
+		res.render('popit_level2');
+	});
+}
